14093e96f4d98a5c8d0d4b8c10bd62bc0295c58e
Boban Acimovic <boban.acimovic@gmail.com>
2020-05-07 18:24:03 +0200

    add tooltip

--- src/BeforeAfter.svelte
+++ src/BeforeAfter.svelte
@@ -1,5 +1,5 @@
 <script>
-  const css = `
+  const css1 = `
 blockquote::before {
   content: open-quote;
 }
@@ -7,14 +7,42 @@ blockquote::after {
   content: close-quote;
 }
 `;
+  const css2 = `
+a[data-tooltip] {
+  color: black;
+  position: relative;
+}
+a[data-tooltip]::after {
+  content: attr(data-tooltip);
+  display: block;
+  position: absolute;
+  background-color: lightgrey;
+  border-radius: 100px;
+  padding: 0.5em 1em;
+  bottom: 0;
+  transform: scale(0);
+  transition: transform 0.25s, bottom 0.25s;
+}
+a[data-tooltip]:hover::after {
+  bottom: 100%;
+  transform: scale(1);
+}
+`;
 </script>
 
 <style>
   .container {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 60vh;
+  }
+  .container div {
+    margin: 3rem;
+  }
+  blockquote {
+    text-align: center;
   }
   blockquote::before {
     content: open-quote;
@@ -22,16 +50,48 @@ blockquote::after {
   blockquote::after {
     content: close-quote;
   }
+  a[data-tooltip] {
+    color: black;
+    position: relative;
+  }
+  a[data-tooltip]::after {
+    content: attr(data-tooltip);
+    display: block;
+    position: absolute;
+    background-color: lightgrey;
+    border-radius: 100px;
+    padding: 0.5em 1em;
+    bottom: 0;
+    transform: scale(0);
+    transition: transform 0.25s, bottom 0.25s;
+  }
+  a[data-tooltip]:hover::after {
+    bottom: 100%;
+    transform: scale(1);
+  }
+  h3 {
+    margin-top: 2rem;
+  }
 </style>
 
 <div class="container">
-  <blockquote
-    cite="https://dzone.com/articles/best-programming-jokes-amp-quotes">
-    Any fool can write code that a computer can understand. Good programmers
-    write code that humans can understand.
-  </blockquote>
+  <div>
+    <blockquote
+      cite="https://dzone.com/articles/best-programming-jokes-amp-quotes">
+      Any fool can write code that a computer can understand. Good programmers
+      write code that humans can understand.
+    </blockquote>
+  </div>
+  <div class="link">
+    <a href="#!" data-tooltip="This is some tooltip">
+      Hover me to see the tooltip
+    </a>
+  </div>
 </div>
 
 <div>
-  <pre>{css}</pre>
+  <h3>Quotes</h3>
+  <pre>{css1}</pre>
+  <h3>Tooltip</h3>
+  <pre>{css2}</pre>
 </div>
