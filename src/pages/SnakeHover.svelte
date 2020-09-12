42ec65cfce60391c2a12ed89f3be8e1a08c222ce
Boban Acimovic <boban.acimovic@gmail.com>
2020-09-13 00:13:43 +0200

    add button to snake hover

--- src/pages/SnakeHover.svelte
+++ src/pages/SnakeHover.svelte
@@ -16,12 +16,14 @@
     align-items: center;
     height: 60vh;
   }
+
   a {
     font-size: 5rem;
     position: relative;
     text-decoration: none;
     color: black;
   }
+
   a::after {
     content: "";
     position: absolute;
@@ -33,13 +35,49 @@
     transform-origin: right;
     transition: transform 350ms ease-in;
   }
+
   a:hover::after {
     transform: scale(1);
     transform-origin: left;
   }
+
+button {
+  margin-top: 2rem;
+  width: 10ch;
+  height: 5rem;
+  background-color: rgba(0,0,255,.8);
+  font-size: 2rem;
+  border: 0;
+  position: relative;
+  z-index: 0;
+  border-radius: .5rem;
+}
+
+button::before {
+  content: '';
+  background-color: red;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -1;
+  left: 0;
+  top: 0;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 500ms ease-in-out;
+  border-radius: .5rem;
+}
+
+button:hover::before {
+  transform: scaleX(1);
+  transform-origin: left;
+}
 </style>
 
-<div class="container"><a href="#!">link</a></div>
+<div class="container">
+  <a href="#!">link</a>
+  <button type="button">Link</button>
+</div>
 
 <div>
   <pre>{css}</pre>
