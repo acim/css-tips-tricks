1dbbab64069a91bfe61731c8b7c5c1773c20df0f
Boban Acimovic <boban.acimovic@gmail.com>
2020-09-13 00:26:14 +0200

    fine tune snake hover

--- src/pages/SnakeHover.svelte
+++ src/pages/SnakeHover.svelte
@@ -5,6 +5,61 @@
   export let nav: crayon.Router;
 
   const css = `
+a {
+  font-size: 2rem;
+  position: relative;
+  text-decoration: none;
+  color: black;
+}
+
+a::after {
+  content: "";
+  position: absolute;
+  border-bottom: 5px solid red;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  transform: scale(0);
+  transform-origin: right;
+  transition: transform 350ms ease-in;
+}
+
+a:hover::after {
+  transform: scale(1);
+  transform-origin: left;
+}
+
+button {
+  margin-top: 2rem;
+  width: 10ch;
+  height: 5rem;
+  background-color: rgba(100, 100, 255, 0.8);
+  font-size: 2rem;
+  border: 0;
+  position: relative;
+  z-index: 0;
+  border-radius: 0.5rem;
+}
+
+button::before {
+  content: "";
+  background-color: rgba(255, 100, 100, 0.8);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -1;
+  left: 0;
+  top: 0;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 500ms ease-in-out;
+  border-radius: 0.5rem;
+}
+
+button:hover::before {
+  transform: scaleX(1);
+  transform-origin: left;
+}
 `;
 </script>
 
@@ -18,7 +73,7 @@
   }
 
   a {
-    font-size: 5rem;
+    font-size: 2rem;
     position: relative;
     text-decoration: none;
     color: black;
@@ -41,42 +96,42 @@
     transform-origin: left;
   }
 
-button {
-  margin-top: 2rem;
-  width: 10ch;
-  height: 5rem;
-  background-color: rgba(0,0,255,.8);
-  font-size: 2rem;
-  border: 0;
-  position: relative;
-  z-index: 0;
-  border-radius: .5rem;
-}
+  button {
+    margin-top: 2rem;
+    width: 10ch;
+    height: 5rem;
+    background-color: rgba(100, 100, 255, 0.8);
+    font-size: 2rem;
+    border: 0;
+    position: relative;
+    z-index: 0;
+    border-radius: 0.5rem;
+  }
 
-button::before {
-  content: '';
-  background-color: red;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: -1;
-  left: 0;
-  top: 0;
-  transform: scaleX(0);
-  transform-origin: right;
-  transition: transform 500ms ease-in-out;
-  border-radius: .5rem;
-}
+  button::before {
+    content: "";
+    background-color: rgba(255, 100, 100, 0.8);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -1;
+    left: 0;
+    top: 0;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 500ms ease-in-out;
+    border-radius: 0.5rem;
+  }
 
-button:hover::before {
-  transform: scaleX(1);
-  transform-origin: left;
-}
+  button:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
 </style>
 
 <div class="container">
-  <a href="#!">link</a>
-  <button type="button">Link</button>
+  <a href="#!">Some super cool link</a>
+  <button type="button">Click Me</button>
 </div>
 
 <div>
